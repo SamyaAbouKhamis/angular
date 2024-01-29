30ce9266e606c74fc8ecca6683bdea406ebb3b2f
SamyaAbouKhamis <rouaba134@gmail.com>
2024-01-29 22:23:47 +0300

    file changing message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   {'path':'', component:HomeComponent},
+   {'path':'about', component:AboutComponent},
+   {'path':'contactus', component:ContactusComponent},
+   {'path':'navbar', component:NavbarComponent}
+];
